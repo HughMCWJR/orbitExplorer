cb71103c557ab229d2b0421c08e43c317f3e89c5
Hugh Williams <hugh.mcwjr@gmail.com>
2021-10-07 12:38:10 -0500

    Added basic point methods

--- Orbit.js
+++ Orbit.js
@@ -7,7 +7,7 @@ class Orbit {
 	constructor(point, sigma = -1, rotationalNumber = null) {
 
 		// Gets array of all points based on point given
-		const points = []
+		let points = []
 
 		for (let i = 0; i < point.string.length; i++) {
 
@@ -17,6 +17,17 @@ class Orbit {
 		}
 
 		this.points = points;
+
+        // If sigma is given, assign it. Otherwise assume smallest possible given the point
+        if (sigma > 1) {
+
+            this.sigma = sigma;
+
+        } else {
+
+            this.sigma = point.getLowestSigma();
+
+        }
 
     }
 
@@ -35,14 +46,14 @@ class Point {
 
 		} else {
 
-			this.string = this.convertFractionToString(value, sigma);
+			this.string = Point.convertFractionToString(value, sigma);
 
 		}
 
 	}
 
 	// Convert Fraction value to String value
-	convertFractionToString(fraction, sigma) {
+	static convertFractionToString(fraction, sigma) {
 
 		if (sigma < 1) {
 
@@ -50,21 +61,77 @@ class Point {
 
 		}
 
+        // Find length of String value
+        // Do this by finding a stringLength and fraction such that 
+        // an equivalent of fraction has the property
+        // fraction.denominator == (sigma ** stringLength) - 1
+        let stringLength = 1;
 
+        while (((sigma ** stringLength) - 1) % fraction.denominator != 0) {
+
+            stringLength++;
+
+        }
+
+        // String value
+        let string = "";
+
+        // Number to multiply by numerator and denominator to get equivalent fraction
+        // That satisfies above property
+        let equivalentFractionMultiplier = ((sigma ** stringLength) - 1) / fraction.denominator;
+
+        // Numerator to be subtracted from to find String value
+        let numerator = fraction.numerator * equivalentFractionMultiplier;
+
+        // Generate String value by converting numerator
+        // to base sigma then adding zeroes on the front
+        // to get desired string length
+        let convertedToBaseSigma = numerator.toString(sigma);
+
+        return "0".repeat(stringLength - convertedToBaseSigma.length) + convertedToBaseSigma;
 
 	}
 
-	get string() {
-		return this.string;
-	}
+    // Get Fraction value of this point
+    // @param: sigma = sigma for Fraction to be based on
+    getFractionValue(sigma) {
+
+        return new Fraction(parseInt(this.string, sigma), (sigma ** this.string.length) - 1);
+
+    }
+
+    // Get lowest possible sigma for this point based on string
+    getLowestSigma() {
+
+        let sigma = 0;
+
+        for (let i = 0; i < this.string.length; i++) {
+
+            let digitValue = parseInt(this.string.substring(i, i + 1));
+
+            if (sigma < digitValue) {
+
+                sigma = digitValue;
+
+            }
+
+        }
+
+        return sigma + 1;
+
+    }
 
 }
 
 class Fraction {
 
     constructor(numerator, denominator) {
-        this.numerator = numerator;
-		this.denominator = denominator;
+
+        let gcd = findGcd(numerator, denominator);
+
+        this.numerator = numerator / gcd;
+		this.denominator = denominator / gcd;
+
 	}
 
 	// Calculate approximate value of fraction
@@ -72,12 +139,14 @@ class Fraction {
         return this.numerator / this.denominator;
     }
 
-    get numerator() {
-        return this.numerator;
-    }
+    static findGcd(a, b) {
 
-    get denominator() {
-        return this.denominator;
+        if (!b) {
+            return a;
+        }
+
+        return this.findGcd(b, a % b);
+
     }
 
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+let fraction = new Fraction(3, 5);
+let point = new Point(fraction, 2);
+alert(point.string);
